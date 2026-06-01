f413c6125f8873331d481f9dafc2d1e0dc390c52
Aaron Tinio <aptinio@gmail.com>
2026-06-01 11:08:54 +0800

    Let there be DefLayout
    mix new def_layout

---
+++ lib/def_layout.ex
@@ -0,0 +1,18 @@
+defmodule DefLayout do
+  @moduledoc """
+  Documentation for `DefLayout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DefLayout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/def_layout_test.exs
@@ -0,0 +1,8 @@
+defmodule DefLayoutTest do
+  use ExUnit.Case
+  doctest DefLayout
+
+  test "greets the world" do
+    assert DefLayout.hello() == :world
+  end
+end
